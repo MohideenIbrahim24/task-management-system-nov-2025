e958bb07e73ae37fbc143cd9ea2a42339de23197
MohideenIbrahim24 <farukthalha23@gmail.com>
2025-12-05 22:18:44 +0530

    client header integrated

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
   // protected readonly title = signal('client');
   title = 'Task Management System';
+  tasks: any[] = [];
+
+  ngOnInit(): void{
+    this.http.get<any>(this.baseUrl + 'Tasks').subscribe({
+      next: tasks => this.tasks = tasks.items,
+      error: err => console.log(err),
+      complete: () => console.log('Request completed')
+    });
+  }
 }
---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatButton
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+
+}
